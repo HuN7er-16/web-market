2653fb5cb5c12a0d9eea7824e4a13dc0e41c77a9
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-01-13 13:22:44 +0330

    admin layouts and routes first things

--- web-market/routes/web.php
+++ web-market/routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Admin
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->namespace('Admin')->group(function(){
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.home');
 });
