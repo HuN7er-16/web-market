8f56119e630fbb6644779b224328b2f677ed9c32
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-01-18 16:39:50 +0330

    admin comment done

--- web-market/resources/views/admin/market/comment/index.blade.php
+++ web-market/resources/views/admin/market/comment/index.blade.php
@@ -1,77 +1,80 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-    <title>برند</title>
+<title>برند</title>
 @endsection
 
 @section('content')
+
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item font-size-12"><a href="#"> خانه</a></li>
-      <li class="breadcrumb-item font-size-12"><a href="#"> بخش فروش</a></li>
-      <li class="breadcrumb-item active font-size-12" aria-current="page"> برند</li>
+      <li class="breadcrumb-item font-size-12"> <a href="#"> خانه</a></li>
+      <li class="breadcrumb-item font-size-12"> <a href="#"> بخش فروش</a></li>
+      <li class="breadcrumb-item font-size-12 active" aria-current="page"> نظرات</li>
     </ol>
   </nav>
+
 
   <section class="row">
     <section class="col-12">
         <section class="main-body-container">
             <section class="main-body-container-header">
-                <h4>
-                    برند
-                </h4>
+                <h5>
+                 نظرات
+                </h5>
             </section>
-            <section class="d-flex justify-content-between align-items-center mt-4 mb-3 pb-2 border-bottom">
-                <a href="{{ route('admin.market.brand.create') }}" class="btn btn-info btn-sm">ایجاد برند </a>
+
+            <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                <a href="#" class="btn btn-info btn-sm disabled">ایجاد نظر </a>
                 <div class="max-width-16-rem">
                     <input type="text" class="form-control form-control-sm form-text" placeholder="جستجو">
                 </div>
             </section>
+
             <section class="table-responsive">
                 <table class="table table-striped table-hover">
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>نام برند </th>
-                            <th>لوگو</th>
+                            <th>کد کاربر</th>
+                            <th>نویسنده نظر</th>
+                            <th>کد کالا</th>
+                            <th>کالا</th>
+                            <th>وضعیت</th>
                             <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <th>1</th>
-                            <td>نمایشگر</td>
-                            <td>کالای الکترونیکی</td>
+                            <td>1231231</td>
+                            <td>حسن خسروجردی</td>
+                            <td>21321312</td>
+                            <td>ایفون 12</td>
+                            <td>در انتظار تایید</td>
                             <td class="width-16-rem text-left">
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> حذف</ش>
+                                <a href="{{ route('admin.market.comment.show') }}" class="btn btn-info btn-sm"><i class="fa fa-eye"></i> نمایش</a>
+                                <button class="btn btn-success btn-sm" type="submit"><i class="fa fa-check"></i> تایید</button>
                             </td>
-
                         </tr>
                         <tr>
                             <th>2</th>
-                            <td>نمایشگر</td>
-                            <td>کالای الکترونیکی</td>
+                            <td>1231231</td>
+                            <td>حسن خسروجردی</td>
+                            <td>21321312</td>
+                            <td>ایفون 12</td>
+                            <td>تایید شده</td>
                             <td class="width-16-rem text-left">
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> حذف</ش>
+                                <a href="{{ route('admin.market.comment.show') }}" class="btn btn-info btn-sm"><i class="fa fa-eye"></i> نمایش</a>
+                                <button class="btn btn-warning btn-sm" type="submit"><i class="fa fa-clock"></i> عدم تایید</button>
                             </td>
-
-                        </tr>
-                        <tr>
-                            <th>3</th>
-                            <td>نمایشگر</td>
-                            <td>کالای الکترونیکی</td>
-                            <td class="width-16-rem text-left">
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> حذف</ش>
-                            </td>
-
                         </tr>
                     </tbody>
                 </table>
             </section>
+
         </section>
     </section>
 </section>
+
 @endsection
