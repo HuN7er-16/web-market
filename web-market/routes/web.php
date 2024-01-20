f5f3c4c45677706f2d9abe6a62192c68f51ec0f0
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-01-20 12:52:22 +0330

    admin order done

--- web-market/routes/web.php
+++ web-market/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\Market\CategoryController;
 use App\Http\Controllers\Admin\Market\CommentController;
 use App\Http\Controllers\Admin\Market\DeliveryController;
 use App\Http\Controllers\Admin\Market\DiscountController;
+use App\Http\Controllers\Admin\Market\OrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -70,6 +71,18 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
             Route::get('/amazing-sale/create', [DiscountController::class, 'amazingSaleCreate'])->name('admin.market.discount.amazingSale.create');
         });
 
+        //order
+        Route::prefix('order')->group(function () {
+            Route::get('/', [OrderController::class, 'all'])->name('admin.market.order.all');
+            Route::get('/new-order', [OrderController::class, 'newOrders'])->name('admin.market.order.newOrders');
+            Route::get('/sending', [OrderController::class, 'sending'])->name('admin.market.order.sending');
+            Route::get('/unpaid', [OrderController::class, 'unpaid'])->name('admin.market.order.unpaid');
+            Route::get('/canceled', [OrderController::class, 'canceled'])->name('admin.market.order.canceled');
+            Route::get('/returned', [OrderController::class, 'returned'])->name('admin.market.order.returned');
+            Route::get('/show', [OrderController::class, 'show'])->name('admin.market.order.show');
+            Route::get('/change-send-status', [OrderController::class, 'changeSendStatus'])->name('admin.market.order.changeSendStatus');
+            Route::get('/change-order-status', [OrderController::class, 'changeOrderStatus'])->name('admin.market.order.changeOrderStatus');
+            Route::get('/cancel-order', [OrderController::class, 'cancelOrder'])->name('admin.market.order.cancelOrder');
+        });
     });
-
 });
