7fde749ec7b153c06f52bba3e5f3837f4773e488
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-01-24 11:16:15 +0330

    database menus and faqs table done

--- web-market/database/migrations/2024_01_24_074243_create_faqs_table.php
+++ web-market/database/migrations/2024_01_24_074243_create_faqs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateFaqsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,13 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('faqs', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->text('question');
+            $table->text('answer');
             $table->string('slug')->unique()->nullable();
-            $table->text('summary');
-            $table->text('body');
-            $table->text('image');
             $table->tinyInteger('status')->default(0);
-            $table->tinyInteger('commentable')->default(0)->comment('0 => uncommentable, 1 => commentable');
             $table->string('tags');
-            $table->timestamp('published_at');
-            $table->foreignId('author_id')->constrained('users');
-            $table->foreignId('category_id')->constrained('post_categories');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -38,6 +32,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('faqs');
     }
 }
