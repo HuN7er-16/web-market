c6506cfc4539dd99f05236d09b49bdaca441e3a3
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-02-04 13:30:56 +0330

    rules and request

--- web-market/app/Http/Requests/Admin/Content/PostCategoryRequest.php
+++ web-market/app/Http/Requests/Admin/Content/PostCategoryRequest.php
@@ -44,4 +44,15 @@ class PostCategoryRequest extends FormRequest
             ];
         }
     }
+
+    public function attributes()
+    {
+        return[
+            'name' => 'نام دسته بندی',
+            'description' => 'توضیحات',
+            'image' => 'تصویر',
+            'status' => 'وضعیت',
+            'tags' => 'تگ ها'
+        ];
+    }
 }
