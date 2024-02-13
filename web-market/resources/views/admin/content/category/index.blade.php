6621360be1053cdc62612707906b5921d49f03ac
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-02-13 11:16:03 +0330

    sweet alert confirm delete

--- web-market/resources/views/admin/content/category/index.blade.php
+++ web-market/resources/views/admin/content/category/index.blade.php
@@ -7,29 +7,31 @@
 @section('content')
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item font-size-12"><a href="#"> خانه</a></li>
-            <li class="breadcrumb-item font-size-12"><a href="#"> بخش محتوا</a></li>
-            <li class="breadcrumb-item active font-size-12" aria-current="page"> دسته بندی</li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> دسته بندی</li>
         </ol>
     </nav>
+
 
     <section class="row">
         <section class="col-12">
             <section class="main-body-container">
                 <section class="main-body-container-header">
-                    <h4>
+                    <h5>
                         دسته بندی
-                    </h4>
+                    </h5>
                 </section>
 
                 @include('admin.alerts.alert-section.success')
 
-                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 pb-2 border-bottom">
+                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
                     <a href="{{ route('admin.content.category.create') }}" class="btn btn-info btn-sm">ایجاد دسته بندی</a>
                     <div class="max-width-16-rem">
                         <input type="text" class="form-control form-control-sm form-text" placeholder="جستجو">
                     </div>
                 </section>
+
                 <section class="table-responsive">
                     <table class="table table-striped table-hover">
                         <thead>
@@ -47,15 +49,17 @@
                         <tbody>
                             @foreach ($postCategories as $postCategory)
                                 <tr>
-                                    <th>{{ $postCategory->id }}</th>
+                                    <th>1</th>
                                     <td>{{ $postCategory->name }}</td>
                                     <td>{{ $postCategory->description }}</td>
                                     <td>{{ $postCategory->slug }}</td>
-                                    <td><img src="{{ asset($postCategory->image) }}" alt="{{ $postCategory->name }}"
-                                            width="50" height="50"></td>
+                                    <td>
+                                        <img src="{{ asset($postCategory->image) }}" alt="" width="50"
+                                            height="50">
+                                    </td>
                                     <td>{{ $postCategory->tags }}</td>
                                     <td>
-                                        <label for="">
+                                        <label>
                                             <input id="{{ $postCategory->id }}"
                                                 onchange="changeStatus({{ $postCategory->id }})"
                                                 data-url="{{ route('admin.content.category.status', $postCategory->id) }}"
@@ -67,23 +71,25 @@
                                             class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
                                         <form class="d-inline"
                                             action="{{ route('admin.content.category.destroy', $postCategory->id) }}"
-                                            method="POST">
+                                            method="post">
                                             @csrf
                                             {{ method_field('delete') }}
-                                            <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash"></i>
-                                                حذف</button>
+                                            <button class="btn btn-danger btn-sm delete" type="submit"><i
+                                                    class="fa fa-trash-alt"></i> حذف</button>
                                         </form>
                                     </td>
                                 </tr>
                             @endforeach
+
+
                         </tbody>
                     </table>
                 </section>
+
             </section>
         </section>
     </section>
 @endsection
-
 @section('script')
     <script type="text/javascript">
         function changeStatus(id) {
@@ -149,4 +155,7 @@
             }
         }
     </script>
+
+
+    @include('admin.alerts.sweetalert.delete-confirm', ['className' => 'delete'])
 @endsection
