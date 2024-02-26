0f97d2ab223510c227f9dafb256b46a9641030b4
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-02-26 11:07:21 +0330

    image cache service

--- web-market/config/image.php
+++ web-market/config/image.php
@@ -23,11 +23,11 @@ return [
             'width' => 800,
             'height' => 600
         ],
-        'medium' => [
+         'medium' => [
             'width' => 400,
             'height' => 300
         ],
-        'small' => [
+         'small' => [
             'width' => 80,
             'height' => 60
         ],
@@ -35,5 +35,29 @@ return [
     ],
 
     'default-current-index-image' => 'medium',
+
+
+ //index size
+    'cache-image-sizes' => [
+        'large' => [
+            'width' => 800,
+            'height' => 600
+        ],
+         'medium' => [
+            'width' => 400,
+            'height' => 300
+        ],
+         'small' => [
+            'width' => 80,
+            'height' => 60
+        ],
+
+    ],
+
+    'default-current-cache-image' => 'medium',
+
+    'image-cache-life-time' => 10,
+
+    'image-not-found' => ''
 
 ];
