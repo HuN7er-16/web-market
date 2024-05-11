7594e089a3b6e68564d9141c6936b42e48997f4d
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-05-11 20:39:27 +0330

    admin-user first part

--- web-market/resources/views/admin/user/admin-user/index.blade.php
+++ web-market/resources/views/admin/user/admin-user/index.blade.php
@@ -45,12 +45,15 @@
                         </tr>
                     </thead>
                     <tbody>
+
+                     @foreach ($admins as $key => $admin)
+
                         <tr>
-                            <th>1</th>
-                            <td>kamran@gmail.com		</td>
-                            <td>09127468392	</td>
-                            <td>کامران	</td>
-                            <td>محمدی	</td>
+                            <th>{{ $key + 1 }}</th>
+                            <td>{{ $admin->email }}</td>
+                            <td>{{ $admin->mobile }}</td>
+                            <td>{{ $admin->first_name }}</td>
+                            <td>{{ $admin->last_name }}</td>
                             <td>سوپر ادمین	</td>
                             <td class="width-22-rem text-left">
                                 <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> نقش</a>
@@ -58,32 +61,9 @@
                                 <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
                             </td>
                         </tr>
-                        <tr>
-                            <th>1</th>
-                            <td>kamran@gmail.com		</td>
-                            <td>09127468392	</td>
-                            <td>کامران	</td>
-                            <td>محمدی	</td>
-                            <td>سوپر ادمین	</td>
-                            <td class="width-22-rem text-left">
-                                <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> نقش</a>
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>1</th>
-                            <td>kamran@gmail.com		</td>
-                            <td>09127468392	</td>
-                            <td>کامران	</td>
-                            <td>محمدی	</td>
-                            <td>سوپر ادمین	</td>
-                            <td class="width-22-rem text-left">
-                                <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> نقش</a>
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
-                            </td>
-                        </tr>
+
+                        @endforeach
+
 
                     </tbody>
                 </table>
